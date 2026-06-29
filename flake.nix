3bccfec248a8c830d80a6f608dcd9c6dca6756b2
Aspharos <ves.hypnos@gmail.com>
2026-06-29 11:02:40 +0800

    feat: 添加 .envrc 和 flake.nix 文件，更新 .gitignore 以包含新文件

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "STSVWB dev shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        pkgs.go
+        pkgs.nodejs
+      ];
+    };
+  };
+}
